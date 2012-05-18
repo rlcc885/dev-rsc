7ffb2d16542cc1a7d1dae55c7ac47e64d14ce077
robertanchivilca@gmail.com <robertanchivilca@gmail.com@7438076b-1481-9fcf-951e-ee4c66ea697b>
2012-05-18 19:46:13 +0000

    prueba bloqueo

--- servir/src/main/java/Batch/Tratamiento.java
+++ servir/src/main/java/Batch/Tratamiento.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-//prueba
+//prueb
 package Batch;
 // Esto es un comentario
 import Batch.Helpers.CreadorDesdeCsv;
