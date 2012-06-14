726a075cf92fc6f57a3368ee1015e8ad1b4e5023
hector06.ch.a@gmail.com <hector06.ch.a@gmail.com@7438076b-1481-9fcf-951e-ee4c66ea697b>
2012-06-14 15:31:51 +0000

    cambio

--- servir/src/main/java/com/tida/servir/pages/ConfigurarAcceso.java
+++ servir/src/main/java/com/tida/servir/pages/ConfigurarAcceso.java
@@ -10,11 +10,12 @@ import org.apache.tapestry5.corelib.components.Zone;
 import org.apache.tapestry5.hibernate.annotations.CommitAfter;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.hibernate.Session;
-
+import org.apache.tapestry5.annotations.Persist;
 import com.tida.servir.base.GeneralPage;
 import com.tida.servir.components.Envelope;
 import com.tida.servir.entities.ConfiguracionAcceso;
 import com.tida.servir.entities.Usuario;
+import java.util.Date;
 
 /**
  * Configurar Acceso de Usuarios
@@ -42,20 +43,48 @@ public class ConfigurarAcceso extends GeneralPage {
 
     @Property
     private ConfiguracionAcceso ca;
+    
+    
+    private int num=0;
 
     @Log
     @CommitAfter
     void onPrepareFromFormularioconfiguraracceso()
     {
-    	ca = (ConfiguracionAcceso) session.load(ConfiguracionAcceso.class, 1L);
+        if(num==2){   
+        }
+        else{
+            ca = (ConfiguracionAcceso) session.load(ConfiguracionAcceso.class, 1L);
+        }
+    	
+    }
+    void onSelectedFromReset() {
+        
+        num=2;
+        ca=new ConfiguracionAcceso();
+
+    }
+    void onSelectedFromCancel() {
+        
+        num=3;
+
     }
     
     @Log
     @CommitAfter
-    Zone onSuccessFromFormularioconfiguraracceso()
+    Object onSuccessFromFormularioconfiguraracceso()
     {
-    	session.saveOrUpdate(ca);
-    	return zone;
+        if(num==2){     
+            
+        }else if(num==3){
+            return "Inicio";
+        } 
+        else{
+            Date fechaactual=new Date();
+            ca.setFec_actualizacion(fechaactual);
+            session.saveOrUpdate(ca);    	
+        }
+        return zone;
     }
 
 }
