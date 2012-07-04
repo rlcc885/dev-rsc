88db02af048b1d0d978000ac9620ba07774b156b
jurguenzambrano@gmail.com <jurguenzambrano@gmail.com@7438076b-1481-9fcf-951e-ee4c66ea697b>
2012-07-04 20:47:56 +0000

    JZM: Actualizando las clases.

--- servir/src/main/java/com/tida/servir/entities/LkBusquedaEntidad.java
+++ servir/src/main/java/com/tida/servir/entities/LkBusquedaEntidad.java
@@ -17,6 +17,16 @@ public class LkBusquedaEntidad implements Serializable {
     @Id
     private long id;
     private String denominacion;
+    private String cue_entidad;
+    private String nivelgobierno;
+    private String sectorgobierno;
+    private String organizacionestado;
+    private String tipoorganismo;
+    private String estado;
+    private String departamento;
+    private String provincia;
+    private String distrito;
+    
     
     public LkBusquedaEntidad() {
     }
@@ -35,6 +45,73 @@ public class LkBusquedaEntidad implements Serializable {
 
     public void setDenominacion(String denominacion) {
         this.denominacion = denominacion;
+    }
+    
+    public String getCue_entidad() {
+        return cue_entidad;
+    }
+
+    public void setCue_entidad(String cue_entidad) {
+        this.cue_entidad = cue_entidad;
+    }
+        
+    public String getNivelgobierno() {
+        return nivelgobierno;
+    }
+
+    public void setNivelgobierno(String nivelgobierno) {
+        this.nivelgobierno = nivelgobierno;
+    }
+        
+    public String getSectorgobierno() {
+        return sectorgobierno;
+    }
+
+    public void setSectorgobierno(String sectorgobierno) {
+        this.sectorgobierno = sectorgobierno;
+    }
+    
+    public String getOrganizacionestado() {
+        return organizacionestado;
+    }
+
+    public void setOrganizacionestado(String organizacionestado) {
+        this.organizacionestado = organizacionestado;
+    }
+      public String getTipoorganismo() {
+        return tipoorganismo;
+    }
+
+    public void setTipoorganismo(String tipoorganismo) {
+        this.tipoorganismo = tipoorganismo;
+    }
+      public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+      public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+      public String getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+      public String geDistrito() {
+        return distrito;
+    }
+
+    public void setDistrito(String distrito) {
+        this.distrito = distrito;
     }
    
 }
