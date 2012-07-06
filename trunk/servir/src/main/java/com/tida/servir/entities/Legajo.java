67e11adb622a3e7005a4752747db9a383026ba3a
renatochavezgirao@gmail.com <renatochavezgirao@gmail.com@7438076b-1481-9fcf-951e-ee4c66ea697b>
2012-07-06 22:22:12 +0000

    RCHG - Varios

--- trunk/servir/src/main/java/com/tida/servir/entities/Legajo.java
+++ trunk/servir/src/main/java/com/tida/servir/entities/Legajo.java
@@ -2,13 +2,7 @@ package com.tida.servir.entities;
 
 import java.util.ArrayList;
 import java.util.List;
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import javax.persistence.Table;
 
 import org.apache.tapestry5.beaneditor.NonVisual;
@@ -20,7 +14,6 @@ public class Legajo {
 //	@Id
 //  @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "system-uuid")
 //  @GenericGenerator(name = "system-uuid", strategy = "uuid")
-
     @NonVisual
     private long id;
     @Validate("required")
