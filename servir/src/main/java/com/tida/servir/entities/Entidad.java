aeb50e434759ae7f2503807d88108f311856c227
jurguenzambrano@gmail.com <jurguenzambrano@gmail.com@7438076b-1481-9fcf-951e-ee4c66ea697b>
2012-07-06 21:46:41 +0000

    JZM: Actualizando las clases.

--- servir/src/main/java/com/tida/servir/entities/Entidad.java
+++ servir/src/main/java/com/tida/servir/entities/Entidad.java
@@ -5,10 +5,7 @@
 package com.tida.servir.entities;
 
 import java.io.Serializable;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import org.apache.tapestry5.beaneditor.NonVisual;
 import org.apache.tapestry5.beaneditor.Validate;
 
@@ -17,6 +14,7 @@ import org.apache.tapestry5.beaneditor.Validate;
  * @author 
  */
 @Entity
+@Table(name = "RSC_ENTIDAD")
 public class Entidad implements Serializable {
 
     public static String ESTADO_BAJA = "Baja";
