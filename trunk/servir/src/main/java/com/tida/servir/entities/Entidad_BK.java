a2665dea6f943ffa3acdc932fd21fe192a486159
jurguenzambrano@gmail.com <jurguenzambrano@gmail.com@7438076b-1481-9fcf-951e-ee4c66ea697b>
2012-07-06 22:02:37 +0000

    JZM: Actualizando entidades.

--- trunk/servir/src/main/java/com/tida/servir/entities/Entidad_BK.java
+++ trunk/servir/src/main/java/com/tida/servir/entities/Entidad_BK.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import org.apache.tapestry5.beaneditor.NonVisual;
 import org.apache.tapestry5.beaneditor.Validate;
 
