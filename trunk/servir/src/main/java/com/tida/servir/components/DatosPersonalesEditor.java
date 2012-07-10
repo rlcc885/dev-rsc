3b56f12b71c06fccfc5c8b8a81deb5c702bd721d
renatochavezgirao@gmail.com <renatochavezgirao@gmail.com@7438076b-1481-9fcf-951e-ee4c66ea697b>
2012-07-10 22:30:22 +0000

    RCHG - Datos Personales

--- trunk/servir/src/main/java/com/tida/servir/components/DatosPersonalesEditor.java
+++ trunk/servir/src/main/java/com/tida/servir/components/DatosPersonalesEditor.java
@@ -68,9 +68,9 @@ public class DatosPersonalesEditor {
     @Property
     private Zone ubigeoDomZone;
     //Datos del formulario (que son persistentes)
-    @Property
-    @Persist
-    private Ubigeo ubigeoNacimiento;
+//    @Property
+//    @Persist
+//    private Ubigeo ubigeoNacimiento;
     @Property
     @Persist
     private Ubigeo ubigeoDomicilio;
@@ -78,7 +78,68 @@ public class DatosPersonalesEditor {
     private ComponentResources _resources;
     @InjectComponent
     private Envelope envelope;
+    
+    @Persist
+    @Property
+    private DatoAuxiliar valtipovia;
+    @Persist
+    @Property
+    private DatoAuxiliar valtipozona;
+    @Persist
+    @Property
+    private DatoAuxiliar valtipodiscapacidad;
+    @Persist
+    @Property
+    private String valconadis;
+    @Persist
+    @Property
+    private String valessalud;
+    @InjectComponent
+    @Property
+    private Zone zonaGeneral;
+    private int elemento=0;
+    
+    
+    @Log
+    @SetupRender
+    private void inicio() {
+        if(actual.getSexo().equals("M")){
+            valsexo="MASCULINO";            
+        }
+        else if(actual.getSexo().equals("F")){
+            valsexo="FEMENINO";
+        }
 
+        domicilioCP = actual.getDomicilioCodigoPostal();
+        domicilioDireccion = actual.getDomicilioDireccion();
+        if (ubigeoDomicilio == null) {
+            ubigeoDomicilio = new Ubigeo();
+        }
+        ubigeoDomicilio.setDepartamento(actual.getCod_dom_dept());
+        ubigeoDomicilio.setProvincia(actual.getCod_dom_prov());
+        ubigeoDomicilio.setDistrito(actual.getCod_dom_dist());
+
+
+//        if (ubigeoNacimiento == null) {
+//            ubigeoNacimiento = new Ubigeo();
+//        }
+//        ubigeoNacimiento.setDepartamento(actual.getCod_ubi_dept());
+//        ubigeoNacimiento.setProvincia(actual.getCod_ubi_prov());
+//        ubigeoNacimiento.setDistrito(actual.getCod_ubi_dist());
+        valtipovia=actual.getTipovia();
+        valtipozona=actual.getTipozona();
+        valtipodiscapacidad=actual.getTipodiscapacidad();
+        valconadis=String.valueOf(actual.getNroCertificadoCONADIS());
+        valessalud=actual.getEsSalud();   
+         if(valconadis.equals("null")){
+            valconadis="0";
+        }
+             
+    }
+    
+    
+    
+    
     @Log
     void onValidateFromformulariodatospersonales() {
         if (actual.getFechaNacimiento().after(new Date())) {
@@ -114,48 +175,101 @@ public class DatosPersonalesEditor {
     public boolean getEditable() {
         return Permisos.puedeEscribir(_usuario, _oi);
     }
-
+    void onSelectedFromSave() {        
+        elemento=1;   
+    }
+    void onSelectedFromReset(){     
+        elemento=2;
+    }
+    
+    void onSelectedFromCancel() {        
+        elemento=3;
+    }
+    
     @Log
     @CommitAfter
     Object onSuccessFromformulariodatospersonales() {
-
-        actual.setPais(pais);
-        actual.setCod_ubi_dept(ubigeoNacimiento.getDepartamento());
-        actual.setCod_ubi_dist(ubigeoNacimiento.getDistrito());
-        actual.setCod_ubi_prov(ubigeoNacimiento.getProvincia());
-        actual.setCod_dom_dept(ubigeoDomicilio.getDepartamento());
-        actual.setCod_dom_dist(ubigeoDomicilio.getDistrito());
-        actual.setCod_dom_prov(ubigeoDomicilio.getProvincia());
-        actual.setDomicilioCodigoPostal(domicilioCP);
-        actual.setDomicilioDireccion(domicilioDireccion);
-
-        List<Trabajador> lTrabajador = session.createCriteria(Trabajador.class).add(Restrictions.eq("nroDocumento", actual.getNroDocumento())).add(Restrictions.ne("id", actual.getId())).list();
-
-        if (lTrabajador.size() > 0) {
-            System.out.println("--------- id actual " + actual.getId() + " lTrabajador size " + lTrabajador.get(0).getId());
-            // No pueden haber 2 trabajadores con el mismo dni
-            formulariodatospersonales.recordError(Errores.ERROR_TRABAJADOR_DNI_EXISTENTE);
-            formulariodatospersonales.recordError("Numero de documento: "
-                    + actual.getNroDocumento());
-            return this;
+        if(elemento==3){
+            
         }
+        else if(elemento==2){
+            
+        }
+        else if(elemento==1){
+    //        actual.setCod_ubi_dept(ubigeoNacimiento.getDepartamento());
+    //        actual.setCod_ubi_dist(ubigeoNacimiento.getDistrito());
+    //        actual.setCod_ubi_prov(ubigeoNacimiento.getProvincia());
+            actual.setCod_dom_dept(ubigeoDomicilio.getDepartamento());
+            actual.setCod_dom_dist(ubigeoDomicilio.getDistrito());
+            actual.setCod_dom_prov(ubigeoDomicilio.getProvincia());
+            actual.setDomicilioCodigoPostal(domicilioCP);
+            actual.setDomicilioDireccion(domicilioDireccion);
+            actual.setTipovia(valtipovia);
+            actual.setTipozona(valtipozona);
+            actual.setTipodiscapacidad(valtipodiscapacidad);
+            actual.setEsSalud(valessalud);
+            actual.setNroCertificadoCONADIS(Integer.parseInt(valconadis));
+            if(valsexo.equals("MASCULINO")){
+                actual.setSexo("M");
+            }
+            else{
+                actual.setSexo("F");
+            }
+            //para dni 
+            List<Trabajador> lTrabajador = session.createCriteria(Trabajador.class).add(Restrictions.eq("nroDocumento", actual.getNroDocumento())).add(Restrictions.ne("id", actual.getId())).list();
 
-        session.saveOrUpdate(actual);
-
-        formulariodatospersonales.clearErrors();
-        envelope.setContents(helpers.Constantes.TRABAJADOR_EDIT_EXITO);
-        return this;
+            if (lTrabajador.size() > 0) {
+                System.out.println("--------- id actual " + actual.getId() + " lTrabajador size " + lTrabajador.get(0).getId());
+                // No pueden haber 2 trabajadores con el mismo dni
+                formulariodatospersonales.recordError(Errores.ERROR_TRABAJADOR_DNI_EXISTENTE);
+                formulariodatospersonales.recordError("Numero de documento: "  + actual.getNroDocumento());
+                return zonaGeneral.getBody();
+            }
+            //para email personal
+            List<Trabajador> lbusqueda = session.createCriteria(Trabajador.class).add(Restrictions.like("emailPersonal", actual.getEmailPersonal())).add(Restrictions.ne("id", actual.getId())).list();
+            if (lbusqueda.size() > 0) {                
+                formulariodatospersonales.recordError("Email Personal ya registrado");   
+                return zonaGeneral.getBody();
+            }
+            //para email laboral
+            List<Trabajador> lbusqueda2 = session.createCriteria(Trabajador.class).add(Restrictions.like("emailLaboral", actual.getEmailLaboral())).add(Restrictions.ne("id", actual.getId())).list();
+            if (lbusqueda2.size() > 0) {                
+                formulariodatospersonales.recordError("Email Laboral ya registrado");   
+                return zonaGeneral.getBody();
+            }
+  
+            session.saveOrUpdate(actual);
+            formulariodatospersonales.clearErrors();
+            envelope.setContents(helpers.Constantes.TRABAJADOR_EDIT_EXITO);
+        }
+        return tresZonas();
         //return datosPersonalesZone.getBody();
         //return tresZonas();
     }
 
     MultiZoneUpdate tresZonas() {
         MultiZoneUpdate mu;
-        mu = new MultiZoneUpdate("ubigeoNacZone", ubigeoNacZone.getBody()).add("ubigeoDomZone", ubigeoDomZone.getBody());
+        mu = new MultiZoneUpdate("ubigeoNacZone", ubigeoNacZone.getBody()).add("ubigeoDomZone", ubigeoDomZone.getBody()).add("zonaGeneral", zonaGeneral.getBody());
         return mu;
     }
 
+    @Log
+    @CommitAfter
     Object onSuccessFromFormUbiNac() {
+        if(valtipodiscapacidad != null && !valtipodiscapacidad.equals("")){
+            if(valtipodiscapacidad.getValor().equals("NO TIENE") || valtipodiscapacidad.getValor().equals("")){
+                vconadis=true;
+                valconadis=null;
+            }        
+            else{
+                vconadis=false;
+            }
+        }
+        else{
+            vconadis=true;
+            valconadis=null;
+        }
+        
         return ubigeoNacZone.getBody();
     }
     @Inject
@@ -166,6 +280,10 @@ public class DatosPersonalesEditor {
     @Property
     @Persist
     private String domicilioDireccion;
+    @Property
+    @Persist
+    private String valsexo;
+    
 
     void onCpChanged() {
         domicilioCP = _request.getParameter("param");
@@ -173,6 +291,14 @@ public class DatosPersonalesEditor {
 
     void onDomChanged() {
         domicilioDireccion = _request.getParameter("param");
+    }
+    
+    void onConaChanged() {
+        valconadis = _request.getParameter("param");
+    }
+    
+    void onEssaChanged() {
+        valessalud = _request.getParameter("param");
     }
 
     // TODO: poner en common
@@ -184,14 +310,28 @@ public class DatosPersonalesEditor {
         return res;
     }
 
-    public List<String> getPaises() {
-        return Helpers.getValorTablaAuxiliar("Paises", session);
+    @Log
+    public GenericSelectModel<DatoAuxiliar> getPaises() {
+        List<DatoAuxiliar> list = Helpers.getDatoAuxiliar("PAISES", null, 0, session);
+        return new GenericSelectModel<DatoAuxiliar>(list, DatoAuxiliar.class, "valor", "id", _access);
     }
 
     public List<String> getNacionalidades() {
         return Helpers.getValorTablaAuxiliar("Nacionalidades", session);
     }
 
+    @Log
+    public GenericSelectModel<DatoAuxiliar> getTipovias() {
+        List<DatoAuxiliar> list = Helpers.getDatoAuxiliar("TIPOVIA", null, 0, session);
+        return new GenericSelectModel<DatoAuxiliar>(list, DatoAuxiliar.class, "valor", "id", _access);
+    }
+    
+    @Log
+    public GenericSelectModel<DatoAuxiliar> getTipozonas() {        
+        List<DatoAuxiliar> list = Helpers.getDatoAuxiliar("TIPOZONA", null, 0, session);
+        return new GenericSelectModel<DatoAuxiliar>(list, DatoAuxiliar.class, "valor", "id", _access);
+    }
+    
     @Log
     public GenericSelectModel<DatoAuxiliar> getEstadoCivil() {
         List<DatoAuxiliar> list = Helpers.getDatoAuxiliar("ESTADOCIVIL", null, 0, session);
@@ -208,24 +348,35 @@ public class DatosPersonalesEditor {
         return Helpers.getValorTablaAuxiliar("SEXO", session);
     }
 
-    public List<String> getGrupoSanguineo() {
-        return Helpers.getValorTablaAuxiliar("GrupoSanguineo", session);
+    @Log
+    public GenericSelectModel<DatoAuxiliar> getGruposanguineos() {
+        List<DatoAuxiliar> list = Helpers.getDatoAuxiliar("GRUPOSANGUINEO", null, 0, session);
+        return new GenericSelectModel<DatoAuxiliar>(list, DatoAuxiliar.class, "valor", "id", _access);
     }
 
-    public List<String> getNivelInstruccion() {
-        return Helpers.getValorTablaAuxiliar("NivelInstrucción", session);
+    @Log
+    public GenericSelectModel<DatoAuxiliar> getNivelinstrucciones() {
+        List<DatoAuxiliar> list = Helpers.getDatoAuxiliar("NIVELINSTRUCCIÓN", null, 0, session);
+        return new GenericSelectModel<DatoAuxiliar>(list, DatoAuxiliar.class, "valor", "id", _access);
+    } 
+    
+    @Log
+    public GenericSelectModel<DatoAuxiliar> getFormacionprofesionales() {
+        List<DatoAuxiliar> list = Helpers.getDatoAuxiliar("FORMACIONPROFESIONAL", null, 0, session);
+        return new GenericSelectModel<DatoAuxiliar>(list, DatoAuxiliar.class, "valor", "id", _access);
     }
 
-    public List<String> getTipoDiscapacidad() {
-        return Helpers.getValorTablaAuxiliar("TipoDiscapacidad", session);
+    @Log
+    public GenericSelectModel<DatoAuxiliar> getTipodiscapacidades() {
+        List<DatoAuxiliar> list = Helpers.getDatoAuxiliar("TIPODISCAPACIDAD", null, 0, session);
+        return new GenericSelectModel<DatoAuxiliar>(list, DatoAuxiliar.class, "valor", "id", _access);
     }
-
-    public List<String> getRegimenPensionario() {
-        return Helpers.getValorTablaAuxiliar("RegPensionarios", session);
+    
+    @Log
+    public GenericSelectModel<DatoAuxiliar> getRegimenpensionarios() {
+        List<DatoAuxiliar> list = Helpers.getDatoAuxiliar("REGPENSIONARIOS", null, 0, session);
+        return new GenericSelectModel<DatoAuxiliar>(list, DatoAuxiliar.class, "valor", "id", _access);
     }
-    @Property
-    @Persist
-    private String pais;
 
     /**
      * Ve si se está en Perú. Si es así se pregunta por los datos de
@@ -233,60 +384,79 @@ public class DatosPersonalesEditor {
      *
      * @return
      */
-    public boolean getEsPeru() {
-        if (pais != null) {
-            return pais.contains("PER");
-        } else {
-            return false;
-        }
-    }
+
     @Inject
     private PropertyAccess _access;
 
-    public Object onChangeOfPais() {
-        pais = _request.getParameter("param");
-        if (!pais.contains("PER")) {
-            ubigeoNacimiento = new Ubigeo();
-        }
-        return ubigeoNacZone.getBody();
-    }
+//    public Object onChangeOfPais() {
+//        pais = _request.getParameter("param");
+//        if (!pais.contains("PER")) {
+////            ubigeoNacimiento = new Ubigeo();
+//        }
+//        return ubigeoNacZone.getBody();
+//    }
 
-    @Log
-    @SetupRender
-    private void setupUbigeos() {
-        if (actual.getNacionalidad() == null) {
-            actual.setNacionalidad("PERÚ");
-        }
-        if (actual.getPais() == null) {
-            pais = "PERÚ";
-        } else {
-            pais = actual.getPais();
-        }
-
-        domicilioCP = actual.getDomicilioCodigoPostal();
-        domicilioDireccion = actual.getDomicilioDireccion();
-        if (ubigeoDomicilio == null) {
-            ubigeoDomicilio = new Ubigeo();
-        }
-        ubigeoDomicilio.setDepartamento(actual.getCod_dom_dept());
-        ubigeoDomicilio.setProvincia(actual.getCod_dom_prov());
-        ubigeoDomicilio.setDistrito(actual.getCod_dom_dist());
-
-
-        if (ubigeoNacimiento == null) {
-            ubigeoNacimiento = new Ubigeo();
-        }
-        ubigeoNacimiento.setDepartamento(actual.getCod_ubi_dept());
-        ubigeoNacimiento.setProvincia(actual.getCod_ubi_prov());
-        ubigeoNacimiento.setDistrito(actual.getCod_ubi_dist());
-
-    }
-
-    public List<String> getTiposDoc() {
+    public List<String> getTiposDoc(){        
         Criteria c = session.createCriteria(DatoAuxiliar.class);
         c.add(Restrictions.eq("nombreTabla", "TipoDocumento"));
         c.add(Restrictions.ne("valor", "Partida de nacimiento (solo a menores)"));
         c.setProjection(Projections.property("valor"));
         return c.list();
+    }
+    //validaciones
+    @Property
+    @Persist
+    private boolean vtipodiscapacidad;
+    @Property
+    @Persist
+    private boolean vconadis;
+    @Property
+    @Persist
+    private boolean vessalud;
+    @Property
+    @Persist
+    private boolean vdatospersonales;
+    @Property
+    @Persist
+    private boolean vdatosubicacion;
+    @Property
+    @Persist
+    private boolean votros;
+    @Property
+    @Persist
+    private boolean vemergencia;
+    
+    
+    void validaciones(){
+        if(valtipodiscapacidad != null && !valtipodiscapacidad.equals("")){
+            vtipodiscapacidad=true;
+            if(valtipodiscapacidad.getValor().equals("NO TIENE") || valtipodiscapacidad.getValor().equals("")){
+                vconadis=true;                
+            }        
+            else{
+                vconadis=false;
+            }
+        }
+        else{
+            vconadis=true;            
+        }
+        if(valessalud!=null && !valessalud.equals("")){
+            vessalud=true;
+        }
+        else{
+            vessalud=false;
+        }
+        if(_usuario.getRol().getId()==1){
+            vdatospersonales=true;
+            vdatosubicacion=false;
+            votros=true;
+            vemergencia=false;            
+        }
+        else if(_usuario.getRol().getId()==2 || _usuario.getRol().getId()==3){
+            vdatospersonales=true;
+            vdatosubicacion=false;
+            votros=false;
+            vemergencia=false;            
+        }
     }
 }
