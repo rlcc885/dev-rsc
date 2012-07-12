58fc9904f88b6cac2469cac71f6b3d90b37d761a
jurguenzambrano@gmail.com <jurguenzambrano@gmail.com@7438076b-1481-9fcf-951e-ee4c66ea697b>
2012-07-12 23:41:14 +0000

    JZM: Actualizando las clases.

--- trunk/servir/src/main/java/com/tida/servir/components/AntecedentesEditor.java
+++ trunk/servir/src/main/java/com/tida/servir/components/AntecedentesEditor.java
@@ -5,11 +5,7 @@
 
 package com.tida.servir.components;
 
-import com.tida.servir.entities.Ant_Laborales;
-import com.tida.servir.entities.Entidad;
-import com.tida.servir.entities.Permisos;
-import com.tida.servir.entities.Trabajador;
-import com.tida.servir.entities.Usuario;
+import com.tida.servir.entities.*;
 
 import helpers.Errores;
 import helpers.Logger;
@@ -20,17 +16,14 @@ import java.util.List;
 import org.apache.tapestry5.BindingConstants;
 import org.apache.tapestry5.PrimaryKeyEncoder;
 import org.apache.tapestry5.ajax.MultiZoneUpdate;
-import org.apache.tapestry5.annotations.Component;
-import org.apache.tapestry5.annotations.InjectComponent;
-import org.apache.tapestry5.annotations.Log;
-import org.apache.tapestry5.annotations.Parameter;
-import org.apache.tapestry5.annotations.Persist;
-import org.apache.tapestry5.annotations.Property;
-import org.apache.tapestry5.annotations.SessionState;
+import org.apache.tapestry5.annotations.*;
 import org.apache.tapestry5.corelib.components.Form;
+import org.apache.tapestry5.corelib.components.Zone;
 import org.apache.tapestry5.hibernate.annotations.CommitAfter;
 import org.apache.tapestry5.ioc.annotations.Inject;
+import org.hibernate.Criteria;
 import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 
 
 /**
@@ -38,146 +31,71 @@ import org.hibernate.Session;
  * @author ale
  */
 public class AntecedentesEditor {
-	// parameters of component
-    @SuppressWarnings("unused")
-    @Parameter(defaultPrefix = BindingConstants.LITERAL)
-    @Property
-    private String zone;
 
-    @Parameter
-    @Property
-    private Trabajador actual;
 
-    @Component(id = "antLaboralesEdit")
-    private Form _form;
 
-    @Property
-    private Ant_Laborales ant_Laborales;
+ 
 
     @Property
     @SessionState
     private Usuario _usuario;
-
     @Property
     @SessionState
     private Entidad _oi;
-
     @Inject
     private Session session;
-    
-    private int elemento=0;
-    
     @InjectComponent
     private Envelope envelope;
-    @Log
-    public PrimaryKeyEncoder<Long, Ant_Laborales> getEncoder()
-    {
-    	return new PrimaryKeyEncoder<Long, Ant_Laborales>()
-    	{
-    		public Long toKey(Ant_Laborales value)
-    		{
-    			return value.getId();
-    		}
-
-    		public void prepareForKeys(List<Long> keys)
-    		{
-    		}
-
-    		public Ant_Laborales toValue(Long key)
-    		{
-    			return (Ant_Laborales) session.get(Ant_Laborales.class, key);
-    		}
-
-    		public Class<Long> getKeyType() {
-    			return Long.class;
-    		}
-    	};
-    }
-
-    public boolean getNoEditable() {
-        return !getEditable();
-    }
-
-    public boolean getEditable() {
-       return Permisos.puedeEscribir(_usuario, _oi);
-    }
-    @Log
-    public boolean getEsTrabajadorEditable(){        
-        if(ant_Laborales != null){
-            if((ant_Laborales.getAgregadoTrabajador() == null)){
-                return true;
-            }
-                
-            if(ant_Laborales.getAgregadoTrabajador() == false){
-                return true;
-            }
-        }
-        return _usuario.getTipo_usuario().equals(Usuario.TRABAJADOR);
-    }
-
-  @Log
-  @CommitAfter
-  Object onSuccessFromAntLaboralesEdit()
-  {
-      for(Ant_Laborales ant : actual.getAnt_Laborales()) {
-                        if((ant.getFec_egreso() != null) && (ant.getFec_ingreso() != null )){
-                            if (ant.getFec_egreso().before(ant.getFec_ingreso())) {
-                                    Logger logger = new Logger();
-                                    logger.loguearError(session, _usuario, String.valueOf(ant.getId()), 
-                                                    Logger.CODIGO_ERROR_FECHA_EGRESO_PREVIA_INGRESO, 
-                                                    Errores.ERROR_FECHA_EGRESO_PREVIA_INGRESO, Logger.TIPO_OBJETO_ANT_LABORALES);
-                            _form.recordError(Errores.ERROR_FECHA_EGRESO_PREVIA_INGRESO);
-                            return this;
-                        }
-                            if(ant.getFec_egreso().after(new Date())) {
-                                    Logger logger = new Logger();
-                                    logger.loguearError(session, _usuario, String.valueOf(ant.getId()), 
-                                                    Logger.CODIGO_ERROR_FECHA_EGRESO_PREVIA_ACTUAL, 
-                                                    Errores.ERROR_FECHA_EGRESO_PREVIA_ACTUAL, Logger.TIPO_OBJETO_ANT_LABORALES);
-                                    _form.recordError(Errores.ERROR_FECHA_EGRESO_PREVIA_ACTUAL);
-                                    return this;
-                            }
-                        }
-                    }
-
-                    _form.clearErrors();
-                    envelope.setContents(helpers.Constantes.ANT_LABORAL_EXITO);
-                    return this;
-        
-  }
-
-  @Log
-  @CommitAfter
-  Object onAddRow()
-  { 
-    Ant_Laborales ant = new Ant_Laborales();
-    if (actual.getAnt_Laborales() == null) {
-        actual.setAnt_Laborales(new ArrayList<Ant_Laborales>());
-    }
-    ant.setTrabajador(actual);
-    if(_usuario.getTipo_usuario().equals(Usuario.TRABAJADOR))
-            ant.setAgregadoTrabajador(Boolean.TRUE);        
-    actual.getAnt_Laborales().add(ant);
-    session.saveOrUpdate(actual);
-    new Logger().loguearOperacion(session, _usuario, String.valueOf(ant.getId()), Logger.CODIGO_OPERACION_ALTA, Logger.RESULTADO_OPERACION_OK, Logger.TIPO_OBJETO_ANT_LABORALES);
-    return ant;
-
-  }
-
-  @Log
-  @CommitAfter
-  void onRemoveRow(Ant_Laborales ant_Laborales)
-  {
-        actual.getAnt_Laborales().remove(ant_Laborales);
-        session.delete(ant_Laborales);
-	new Logger().loguearOperacion(session, _usuario, String.valueOf(ant_Laborales.getId()), Logger.CODIGO_OPERACION_BAJA, Logger.RESULTADO_OPERACION_OK, Logger.TIPO_OBJETO_ANT_LABORALES);
-  }
-
-     Object onFailure() {
-          return this;
-    }
+   
+    
+    @Component(id = "formulariomensajes")
+    private Form formulariomensajes;
+    @InjectComponent
+    private Zone mensajesZone;  
+    
+    @InjectComponent
+    private Zone antLaboralZone;
+    
+    private int elemento=0;
      
-     
+    @Parameter
+    @Property
+    private Trabajador actual;
+
+    @Persist
+    @Property
+    private Ant_Laborales ant_Laborales;
+    
+    @Property
+    @Persist
+    private boolean bvalidausuario;
+    
+    //Listado de experiencia laboral
+    @InjectComponent
+    private Zone listaAntLoboralZone;
+    @Persist
+    @Property
+    private Ant_Laborales listaantlaborales;
+    
+    //Inicio de lac carga de la pagina
+    @Log
+    @SetupRender
+    private void inicio() {
+            ant_Laborales = new Ant_Laborales();
+            if(_usuario.getRol().getId()==2 || _usuario.getRol().getId()==3){
+                bvalidausuario=true;
+            }else{
+                bvalidausuario=false;
+            }
+    }
+    
+    @Log
+    public List<Ant_Laborales> getListadoAntLaborales() {
+        Criteria c = session.createCriteria(Ant_Laborales.class);
+        c.add(Restrictions.eq("trabajador",actual));  
+        return c.list();
+    }
+    
     void onSelectedFromCancel() {
         elemento=2;
     }
@@ -188,11 +106,28 @@ public class AntecedentesEditor {
     
     @Log
     @CommitAfter    
-    Object onSuccessFromFormulariobotones() {
+    Object onSuccessFromFormularioantlaboral() {
+        if (ant_Laborales.getFec_egreso().before(ant_Laborales.getFec_ingreso()) || ant_Laborales.getFec_egreso().equals(ant_Laborales.getFec_ingreso())){
+            envelope.setContents("Las fecha de ingreso debe ser menor a la fecha de egreso");  
+   
+        }else{
+          ant_Laborales.setTrabajador(actual);
+            ant_Laborales.setEntidad(_oi);
+            session.saveOrUpdate(ant_Laborales);
+            envelope.setContents(helpers.Constantes.ANT_LABORAL_EXITO);
+        }
+        return new MultiZoneUpdate("mensajesZone", mensajesZone.getBody())                             
+                    .add("listaAntLoboralZone", listaAntLoboralZone.getBody());   
         
+    }
+    
+    @Log
+    @CommitAfter    
+    Object onSuccessFromFormulariobotones() {
+         System.out.println("1: "+elemento );
         if(elemento==1){
-            ant_Laborales=null;
-            return this;
+            ant_Laborales=new Ant_Laborales();;
+            return  antLaboralZone.getBody();
         }else if(elemento==2){
             return "Busqueda";
         }else{    
@@ -200,6 +135,21 @@ public class AntecedentesEditor {
         }
         
     }
+    
+    @Log
+    Object onActionFromEditar(Ant_Laborales antLab) {        
+        ant_Laborales=antLab;
+           return antLaboralZone.getBody(); 
+    }
+    
+    @Log
+    @CommitAfter        
+    Object onActionFromEliminar(Ant_Laborales antLab) {
+        session.delete(antLab);
+           return listaAntLoboralZone.getBody();
+    }
+    
+ 
     
 }
 
