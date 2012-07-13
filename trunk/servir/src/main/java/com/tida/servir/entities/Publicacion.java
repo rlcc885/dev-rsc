381c2b0f64715e0face98b0e96ae46c1fb95199a
jurguenzambrano@gmail.com <jurguenzambrano@gmail.com@7438076b-1481-9fcf-951e-ee4c66ea697b>
2012-07-13 23:49:07 +0000

    JZM: Actualizando las clases.

--- trunk/servir/src/main/java/com/tida/servir/entities/Publicacion.java
+++ trunk/servir/src/main/java/com/tida/servir/entities/Publicacion.java
@@ -17,41 +17,70 @@ import org.apache.tapestry5.beaneditor.NonVisual;
 @Table(name = "RSC_PUBLICACION")
 public class Publicacion implements Serializable {
 
-    public static String CLASE_PUBLICACION = "PUBLICACION";
-    public static String CLASE_INVESTIGACION = "INVESTIGACION";
-//	@Id
-//  @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "system-uuid")
-//  @GenericGenerator(name = "system-uuid", strategy = "uuid")
     @NonVisual
-    @Id
-    @SequenceGenerator(name = "MYENTITY_SEQ", sequenceName = "PUBLICACION_ID_SEQ", allocationSize=1)
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MYENTITY_SEQ")
-    private Long id;
-//    @ManyToOne(optional = false)
-    private Trabajador trabajador;
-    private String clase;
-    private String tipo;
+    private Long id;  
+    private DatoAuxiliar clasepublicacion;
+    private DatoAuxiliar tipo;
     private String titulo;
+    private String descripcion;
     @Temporal(TemporalType.DATE)
     private Date fecha;
     private boolean agregadoTrabajador;
-    private boolean entidad;
     private Boolean validado;
-
-    public Boolean getValidado() {
-        return validado;
+    
+    private Entidad entidad;
+    private Trabajador trabajador;
+    
+    @Id
+    @GeneratedValue
+    public Long getId() {
+        return id;
     }
 
-    public void setValidado(Boolean validado) {
-        this.validado = validado;
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    @ManyToOne
+    public DatoAuxiliar getClasePublicacion() {
+        return clasepublicacion;
     }
 
-    public boolean getEntidad() {
-        return entidad;
+    public void setClasePublicacion(DatoAuxiliar clasepublicacion) {
+        this.clasepublicacion = clasepublicacion;
+    }
+    
+    @ManyToOne
+    public DatoAuxiliar getTipo() {
+        return tipo;
     }
 
-    public void setEntidad(boolean entidad) {
-        this.entidad = entidad;
+    public void setTipo(DatoAuxiliar tipo) {
+        this.tipo = tipo;
+    }
+    
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
     }
     
     public Boolean getAgregadoTrabajador() {
@@ -60,10 +89,36 @@ public class Publicacion implements Serializable {
 
     public void setAgregadoTrabajador(Boolean agregadoTrabajador) {
         this.agregadoTrabajador = agregadoTrabajador;
+    
+    }
+    
+    public Boolean getValidado() {
+        return validado;
     }
 
+    public void setValidado(Boolean validado) {
+        this.validado = validado;
+    }
+
+    @ManyToOne(optional = false)
+    public Trabajador getTrabajador() {
+        return trabajador;
+    }
+
+    public void setTrabajador(Trabajador trabajador) {
+        this.trabajador = trabajador;
+    }
     
-    @Override
+    @ManyToOne(optional = false)
+    public Entidad getEntidad() {
+        return entidad;
+    }
+
+    public void setEntidad(Entidad entidad) {
+        this.entidad = entidad;
+    }
+    
+     @Override
     public boolean equals(Object obj) {
         if (obj == null) {
             return false;
@@ -81,65 +136,8 @@ public class Publicacion implements Serializable {
     @Override
     public int hashCode() {
         int hash = 7;
-        hash = 79 * hash + (this.id != null ? this.id.hashCode() : 0);
+        hash = 67 * hash + (this.id != null ? this.id.hashCode() : 0);
         return hash;
     }
-
-    public String getClase() {
-        return clase;
-    }
-
-    public void setClase(String clase) {
-        this.clase = clase;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public Date getFecha() {
-        return fecha;
-    }
-
-    public void setFecha(Date fecha) {
-        this.fecha = fecha;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    @ManyToOne(optional = false,  fetch = FetchType.EAGER)
-    public Trabajador getTrabajador() {
-        return trabajador;
-    }
-
-    public void setTrabajador(Trabajador trabajador) {
-        this.trabajador = trabajador;
-    }
-    private String descripcion;
+    
 }
