d0d62129a211e5d10e9d138295e977f2e430c51e
jurguenzambrano@gmail.com <jurguenzambrano@gmail.com@7438076b-1481-9fcf-951e-ee4c66ea697b>
2012-07-17 23:00:10 +0000

    JZM: Actualizando las clases.

--- servir/src/main/java/com/tida/servir/pages/CambiarClave.java
+++ servir/src/main/java/com/tida/servir/pages/CambiarClave.java
@@ -10,6 +10,7 @@ import com.tida.servir.entities.Entidad_BK;
 import com.tida.servir.entities.Usuario;
 import helpers.Encriptacion;
 import java.util.Date;
+import org.apache.tapestry5.ajax.MultiZoneUpdate;
 import org.apache.tapestry5.annotations.*;
 import org.apache.tapestry5.corelib.components.Form;
 import org.apache.tapestry5.corelib.components.Zone;
@@ -29,6 +30,8 @@ public class CambiarClave extends GeneralPage {
     @InjectComponent
     private Zone zone;
     @InjectComponent
+    private Zone zone2;
+    @InjectComponent
     private Envelope envelope;
     @Property
     @SessionState
@@ -43,6 +46,11 @@ public class CambiarClave extends GeneralPage {
     private String newPass2;
     @Persist
     private boolean cambioForzado;
+    
+    @Property
+    private String verificacion;
+    
+    private int elemento=0;
 
     public Boolean getNoEsAdmSystema() {
         if ((_usuario.getTipo_usuario().equals(Usuario.ADMINSISTEMA)) || (_usuario.getTipo_usuario().equals(Usuario.ADMINGRAL))) {
@@ -73,38 +81,54 @@ public class CambiarClave extends GeneralPage {
 
     @Log
     @CommitAfter
-    Zone onSuccessFromFormularioCambioClave() {
-        if (!Encriptacion.encriptaEnMD5(oldPass).equals(_usuario.getMd5Clave())) {
-            formulariocambioclave.recordError("Clave actual ingresada incorrecta.");
-            //envelope.setContents("Clave actual ingresada incorrecta.");
-            return zone;
+    Object onSuccessFromFormularioCambioClave() {
+         if(elemento==1){
+            return  "CambiarClave";
         }
+         else{ 
+            if (!Encriptacion.encriptaEnMD5(oldPass).equals(_usuario.getMd5Clave())) {
+                verificacion="";
+                formulariocambioclave.recordError("Clave actual ingresada incorrecta.");
+                //envelope.setContents("Clave actual ingresada incorrecta.");
+                 return new MultiZoneUpdate("zone", zone.getBody())                             
+                    .add("zone2", zone2.getBody()); 
+            }
 
-        if (oldPass.equals(Encriptacion.encriptaEnMD5(newPass1))) {
-            formulariocambioclave.recordError("La nueva clave debe ser diferente a la actual.");
-            //envelope.setContents("Clave actual ingresada incorrecta.");
-            return zone;
-        }
+            if (oldPass.equals(Encriptacion.encriptaEnMD5(newPass1))) {
+                verificacion="";
+                formulariocambioclave.recordError("La nueva clave debe ser diferente a la actual.");
+                //envelope.setContents("Clave actual ingresada incorrecta.");
+                return new MultiZoneUpdate("zone", zone.getBody())                             
+                    .add("zone2", zone2.getBody()); 
+            }
 
-        if (!newPass1.equals(newPass2)) {
-            formulariocambioclave.recordError("Las claves ingresadas deben ser iguales.");
-            //		envelope.setContents("Las claves ingresadas deben ser iguales.");
-            return zone;
-        }
+            if (!newPass1.equals(newPass2)) {
+                verificacion="";
+                formulariocambioclave.recordError("Las claves ingresadas deben ser iguales.");
+                //		envelope.setContents("Las claves ingresadas deben ser iguales.");
+                return new MultiZoneUpdate("zone", zone.getBody())                             
+                    .add("zone2", zone2.getBody()); 
+            }
 
 
-        envelope.setContents("Clave modificada con éxito.");
-        if (this.cambioForzado) {
-            envelope.setContents("Clave modificada con éxito. <a href='/servir'>Reingresar al sistema.</a>");
-        }
-        // formulariocambioclave.recordError("Clave modificada con éxito");
-        _usuario.setMd5Clave(Encriptacion.encriptaEnMD5(newPass1));
-        _usuario.setClave(newPass1);
-        _usuario.setUltimo_cambio_clave(new Date());
-        _usuario.setIntentos_fallidos(0L);
-        session.saveOrUpdate(_usuario);
+            envelope.setContents("Clave modificada con éxito.");
+            if (this.cambioForzado) {
+                envelope.setContents("Clave modificada con éxito. <a href='/servir'>Reingresar al sistema.</a>");
+            }
+            // formulariocambioclave.recordError("Clave modificada con éxito");
+            _usuario.setMd5Clave(Encriptacion.encriptaEnMD5(newPass1));
+            _usuario.setClave(newPass1);
+            _usuario.setUltimo_cambio_clave(new Date());
+            _usuario.setIntentos_fallidos(0L);
+            session.saveOrUpdate(_usuario);
 
-        return zone;
-        // return this;
+            return new MultiZoneUpdate("zone", zone.getBody())                             
+                    .add("zone2", zone2.getBody()); 
+            // return this;
+         }
+    }
+    
+     void onSelectedFromReset() {
+         elemento=1;
     }
 }
