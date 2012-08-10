dc08c68645f34b70429e2483afae8bef82a344ad
jurguenzambrano@gmail.com <jurguenzambrano@gmail.com@7438076b-1481-9fcf-951e-ee4c66ea697b>
2012-08-10 21:43:01 +0000

    JZM: Actualizando páginas.

--- trunk/servir/src/main/java/com/tida/servir/components/Layout.java
+++ trunk/servir/src/main/java/com/tida/servir/components/Layout.java
@@ -17,7 +17,7 @@ import org.apache.tapestry5.services.javascript.StylesheetOptions;
 "context:layout/tabs.css"})
 //        @IncludeJavaScriptLibrary("context:layout/protofish.js")
 @IncludeJavaScriptLibrary({"context:layout/jquery.tools.min.js", "context:layout/protofish-min.js"
-                            , "context:layout/jALDropDown1.7.3.js","context:layout/navegador.js","context:layout/selectorder.js"})
+                            , "context:layout/jALDropDown1.7.3.js","context:layout/navegador.js","context:layout/selectorder.js","context:layout/popcalendar.js"})
 public class Layout
 {
 	/** The page title, for the <title> element and the <h1> element. */
