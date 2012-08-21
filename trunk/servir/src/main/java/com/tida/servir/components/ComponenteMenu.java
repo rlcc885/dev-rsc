82d88430e3d539da43d2b94a724f5021c699301a
jurguenzambrano@gmail.com <jurguenzambrano@gmail.com@7438076b-1481-9fcf-951e-ee4c66ea697b>
2012-08-21 21:56:25 +0000

    Wilson : Actualizando las clases.

--- trunk/servir/src/main/java/com/tida/servir/components/ComponenteMenu.java
+++ trunk/servir/src/main/java/com/tida/servir/components/ComponenteMenu.java
@@ -56,6 +56,13 @@ public class ComponenteMenu {
     @Property
     private UsuarioAcceso opcion;
 
+    
+    public Object onSalir()
+    {
+     _usuario=null;
+     return "Index";
+    }
+    
     public List getOpcionesMenu() {
         Query query = session.getNamedQuery("callSpUsuarioAcceso");
         query.setParameter("in_nrodocumento", _usuario.getTrabajador().getNroDocumento());
@@ -76,7 +83,7 @@ public class ComponenteMenu {
         List result = query.list();
         for (int i = 0; i < result.size(); i++) {
             UsuarioAcceso stock = (UsuarioAcceso) result.get(i);
-            //System.out.println(stock.getDescmenu()+stock.getActivo().toString());
+   
         }
         return result;
     }
@@ -108,199 +115,7 @@ public class ComponenteMenu {
     public String getNombreUsuario(){
         return _usuario.getTrabajador().getApellidoPaterno()+" "+_usuario.getTrabajador().getApellidoMaterno()+", "+_usuario.getTrabajador().getNombres()+" - "+_usuario.getTrabajador().getEntidad().getDenominacion();
     }
-/*
-    public Set<Accesos.MENUPADRE> getPagesMenuPadre() {
-        return menues.keySet();
-    }
 
-    public List<Accesos.MENUHIJO> getPagesMenuHijos() {
-        return menues.get(currentPadre);
-    }
-
-    public String getCurrentPadrePageName() {
-        return Accesos.getPagePadreName(currentPadre);
-    }
-
-    public String getCurrentHijoPageName() {
-        return Accesos.getPageHijoName(currentHijo);
-    }
-
-    public boolean getTieneHijos() {
-        return menues.get(currentPadre) != null;
-    }
-
-    public String getPagePadreName() {
-        return Accesos.getNombrePadre(currentPadre);
-    }
-
-    public String getPageHijoName() {
-        return Accesos.getNombreHijo(currentHijo);
-    }
-
-    public String getIsActivePadre() {
-        List<Accesos.MENUHIJO> hijos = menues.get(currentPadre);
-        Boolean esta = false;
-
-        if (hijos != null) {
-            for (Accesos.MENUHIJO hijo : hijos) {
-                esta = esta || ((Accesos.getPageHijoName(hijo)).equalsIgnoreCase(resources.getPageName()));
-            }
-        }
-        return esta ? "selectedPadre" : null;
-    }
-
-    public String getIsActiveHijo() {
-        return (getCurrentHijoPageName().equalsIgnoreCase(resources.getPageName())) ? "selectedHijo" : null;
-    }
-
-    public String getNombres() {
-        return _usuario.getNombres();
-    }
-
-    public String getApellidos() {
-        return _usuario.getApellidos();
-    }
-
-    public String getTipoUsuario() {
-        return _usuario.getTipo_usuario();
-    }
-
-    @SetupRender
-    void cargoDatos() {
-
-        System.out.println("------------------------Página: " + resources.getPageName());
-
-        menues = new EnumMap<Accesos.MENUPADRE, List<Accesos.MENUHIJO>>(Accesos.MENUPADRE.class);
-        List<Accesos.MENUHIJO> menuhijo;
-
-        if (_usuario.getTipo_usuario().equals(Usuario.ADMINGRAL)) {
-            menuhijo = new ArrayList<Accesos.MENUHIJO>();
-            menuhijo.add(Accesos.MENUHIJO.AdministrarUsuarios);
-            menuhijo.add(Accesos.MENUHIJO.ReportesUsuarios);
-            menues.put(Accesos.MENUPADRE.Usuarios, menuhijo);
-        }
-
-        if (_usuario.getTipo_usuario().equals(Usuario.ADMINLOCAL)) {
-            menuhijo = new ArrayList<Accesos.MENUHIJO>();
-            menuhijo.add(Accesos.MENUHIJO.AdministrarUsuarios);
-            menuhijo.add(Accesos.MENUHIJO.ReportesUsuarios);
-            menues.put(Accesos.MENUPADRE.Usuarios, menuhijo);
-        }
-
-        if (_usuario.getTipo_usuario().equals(Usuario.ADMINSISTEMA)) {
-
-            menuhijo = new ArrayList<Accesos.MENUHIJO>();
-            menuhijo.add(Accesos.MENUHIJO.AdministracionEntidades);
-            menuhijo.add(Accesos.MENUHIJO.ReportesEntidades);
-            menues.put(Accesos.MENUPADRE.Entidades, menuhijo);
-
-            menuhijo = new ArrayList<Accesos.MENUHIJO>();
-            menuhijo.add(Accesos.MENUHIJO.AdministracionTablas);
-            menues.put(Accesos.MENUPADRE.TablasAuxiliares, menuhijo);
-
-            menues.put(Accesos.MENUPADRE.DeteccionIntrusion, null);
-            menues.put(Accesos.MENUPADRE.Parametros, null);
-            menues.put(Accesos.MENUPADRE.ProcesoBatch, null);
-        }
-
-
-        if (_usuario.getTipo_usuario().equals(Usuario.OPERADORABMSERVIR)) {
-
-            menuhijo = new ArrayList<Accesos.MENUHIJO>();
-            menuhijo.add(Accesos.MENUHIJO.SeleccionEntidades);
-            menuhijo.add(Accesos.MENUHIJO.DescargaArchivoCSV);
-            menues.put(Accesos.MENUPADRE.Entidades, menuhijo);
-
-            menuhijo = new ArrayList<Accesos.MENUHIJO>();
-            menuhijo.add(Accesos.MENUHIJO.UnidadesOrganicas);
-            menuhijo.add(Accesos.MENUHIJO.Cargos);
-            menuhijo.add(Accesos.MENUHIJO.CambioUOEntidad);
-            menuhijo.add(Accesos.MENUHIJO.ReportesEstructura);
-            menuhijo.add(Accesos.MENUHIJO.ReportesNivel);
-            menues.put(Accesos.MENUPADRE.Organigrama, menuhijo);
-
-            menuhijo = new ArrayList<Accesos.MENUHIJO>();
-            menuhijo.add(Accesos.MENUHIJO.AltaTrabajador);
-            menuhijo.add(Accesos.MENUHIJO.ModificacionTrabajador);
-            menuhijo.add(Accesos.MENUHIJO.BajaTrabajador);
-            menuhijo.add(Accesos.MENUHIJO.ReportesTrabajador);
-            menues.put(Accesos.MENUPADRE.Trabajadores, menuhijo);
-
-            menuhijo = new ArrayList<Accesos.MENUHIJO>();
-            menuhijo.add(Accesos.MENUHIJO.AdministracionConceptos);
-            menues.put(Accesos.MENUPADRE.ConceptosRemunerativos, menuhijo);
-
-            menues.put(Accesos.MENUPADRE.ProcesoBatchUpload, null);
-        }
-
-        if (_usuario.getTipo_usuario().equals(Usuario.OPERADORABMLOCAL)) {
-
-            menuhijo = new ArrayList<Accesos.MENUHIJO>();
-            menuhijo.add(Accesos.MENUHIJO.UnidadesOrganicas);
-            menuhijo.add(Accesos.MENUHIJO.Cargos);
-            menuhijo.add(Accesos.MENUHIJO.ReportesEstructura);
-            menuhijo.add(Accesos.MENUHIJO.ReportesNivel);
-            menuhijo.add(Accesos.MENUHIJO.DescargaArchivoCSV);
-            menues.put(Accesos.MENUPADRE.Organigrama, menuhijo);
-
-            menuhijo = new ArrayList<Accesos.MENUHIJO>();
-            menuhijo.add(Accesos.MENUHIJO.AltaTrabajador);
-            menuhijo.add(Accesos.MENUHIJO.ModificacionTrabajador);
-            menuhijo.add(Accesos.MENUHIJO.BajaTrabajador);
-            menuhijo.add(Accesos.MENUHIJO.ReportesTrabajador);
-            menues.put(Accesos.MENUPADRE.Trabajadores, menuhijo);
-
-            menues.put(Accesos.MENUPADRE.ProcesoBatchUpload, null);
-        }
-
-        if (_usuario.getTipo_usuario().equals(Usuario.OPERADORLECTURALOCAL)) {
-
-            menuhijo = new ArrayList<Accesos.MENUHIJO>();
-            menuhijo.add(Accesos.MENUHIJO.UnidadesOrganicas);
-            menuhijo.add(Accesos.MENUHIJO.Cargos);
-            menuhijo.add(Accesos.MENUHIJO.ReportesEstructura);
-            menuhijo.add(Accesos.MENUHIJO.ReportesNivel);
-            menues.put(Accesos.MENUPADRE.Organigrama, menuhijo);
-
-            menuhijo = new ArrayList<Accesos.MENUHIJO>();
-            menuhijo.add(Accesos.MENUHIJO.ModificacionTrabajador);
-            menuhijo.add(Accesos.MENUHIJO.ReportesTrabajador);
-            menues.put(Accesos.MENUPADRE.Trabajadores, menuhijo);
-        }
-
-        if (_usuario.getTipo_usuario().equals(Usuario.OPERADORANALISTA)) {
-            menuhijo = new ArrayList<Accesos.MENUHIJO>();
-            menuhijo.add(Accesos.MENUHIJO.SeleccionEntidades);
-            menues.put(Accesos.MENUPADRE.Entidades, menuhijo);
-
-            menuhijo = new ArrayList<Accesos.MENUHIJO>();
-            menuhijo.add(Accesos.MENUHIJO.UnidadesOrganicas);
-            menuhijo.add(Accesos.MENUHIJO.Cargos);
-            menuhijo.add(Accesos.MENUHIJO.ReportesEstructura);
-            menuhijo.add(Accesos.MENUHIJO.ReportesNivel);
-            menues.put(Accesos.MENUPADRE.Organigrama, menuhijo);
-
-            menuhijo = new ArrayList<Accesos.MENUHIJO>();
-            menuhijo.add(Accesos.MENUHIJO.ModificacionTrabajador);
-            menuhijo.add(Accesos.MENUHIJO.ReportesTrabajador);
-            menues.put(Accesos.MENUPADRE.Trabajadores, menuhijo);
-
-            menuhijo = new ArrayList<Accesos.MENUHIJO>();
-            menuhijo.add(Accesos.MENUHIJO.AdministracionConceptos);
-            menues.put(Accesos.MENUPADRE.ConceptosRemunerativos, menuhijo);
-        }
-
-
-        if (_usuario.getTipo_usuario().equals(Usuario.TRABAJADOR)) {
-            menuhijo = new ArrayList<Accesos.MENUHIJO>();
-            menuhijo.add(Accesos.MENUHIJO.TrabajadorPage);
-            menues.put(Accesos.MENUPADRE.Trabajadores, menuhijo);
-        }
-
-        menues.put(Accesos.MENUPADRE.CambiarClave, null);
-        menues.put(Accesos.MENUPADRE.Salir, null);
-    }
-*/
     StreamResponse onActionFromReturnStreamResponse() {
         return new StreamResponse() {
 
