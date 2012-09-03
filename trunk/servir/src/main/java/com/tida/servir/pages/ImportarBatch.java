3e075360237670a31b1963e94986d2086bfc4153
renatochavezgirao@gmail.com <renatochavezgirao@gmail.com@7438076b-1481-9fcf-951e-ee4c66ea697b>
2012-09-03 02:16:38 +0000

    RCHG

--- trunk/servir/src/main/java/com/tida/servir/pages/ImportarBatch.java
+++ trunk/servir/src/main/java/com/tida/servir/pages/ImportarBatch.java
@@ -9,8 +9,10 @@ import com.tida.servir.base.GeneralPage;
 import com.tida.servir.components.Envelope;
 import com.tida.servir.entities.*;
 import com.tida.servir.services.GenericSelectModel;
+import helpers.Encriptacion;
 import helpers.Errores;
 import helpers.Helpers;
+import java.io.File;
 import java.util.LinkedList;
 import java.util.List;
 import org.apache.tapestry5.ComponentResources;
@@ -33,6 +35,7 @@ import org.hibernate.Session;
 import org.hibernate.criterion.Restrictions;
 
 import java.util.ArrayList;
+import org.apache.tapestry5.services.ApplicationGlobals;
 import org.apache.tapestry5.upload.services.UploadedFile;
 import org.hibernate.Query;
 
@@ -68,13 +71,41 @@ public class ImportarBatch extends GeneralPage{
     @Persist
     @Property
     private String tipoproceso;
+    @Property
+    @Persist
     private UploadedFile file;
+    @InjectComponent
+    private Envelope envelope;
+    @Inject
+    private ApplicationGlobals globals;
+    @Component(id = "primerform")
+    private Form primerform;
+    
     
     @Log
     @SetupRender
     private void inicio() {
 
     }
-
+    
+    @CommitAfter
+    Object onSuccessFromPrimerform() {
+        File copied;
+        if (file == null) {
+            primerform.recordError("Seleccione archivo a subir.");
+            return this;
+        }
+//        String path = globals.getServletContext().getRealPath("/") + "images/logotipo/";
+        String path="D:/5to ciclo/";
+        String nombreArchivo = Encriptacion.encriptaEnMD5( String.valueOf(entidadUE.getId()) ) + file.getFileName().substring(file.getFileName().length() - 4);
+        File nuevo = new File(path + nombreArchivo);
+        copied = new File(path);
+        if (!copied.exists()) {
+            copied.mkdirs();
+        }
+        file.write(nuevo);
+        envelope.setContents("Archivo Importado");
+        return this;
+    }
 
 }
