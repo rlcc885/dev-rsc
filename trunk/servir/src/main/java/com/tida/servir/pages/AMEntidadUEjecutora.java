3d530d4dbca17583d5d71d34737e91c36944b1c7
jurguenzambrano@gmail.com <jurguenzambrano@gmail.com@7438076b-1481-9fcf-951e-ee4c66ea697b>
2012-09-06 00:24:50 +0000

    wilson actualizando

--- trunk/servir/src/main/java/com/tida/servir/pages/AMEntidadUEjecutora.java
+++ trunk/servir/src/main/java/com/tida/servir/pages/AMEntidadUEjecutora.java
@@ -1071,10 +1071,11 @@ public class AMEntidadUEjecutora extends GeneralPage {
         Criteria c = session.createCriteria(LkBusquedaTrabajador.class);
         c.add(Restrictions.eq("entidad_id",entidadUE.getId()));
         if (nombreTrabajador != null) {
-            c.add(Restrictions.disjunction().add(Restrictions.like("nombretrabajador","%"+ nombreTrabajador + "%").ignoreCase()).add(Restrictions.like("nombretrabajador", nombreTrabajador.replaceAll("ñ", "n") + "%").ignoreCase()).add(Restrictions.like("nombretrabajador", nombreTrabajador.replaceAll("n", "ñ") + "%").ignoreCase()));
+            c.add(Restrictions.disjunction().add(Restrictions.like("nombretrabajador","%"+ nombreTrabajador + "%").ignoreCase()).add(Restrictions.like("nombretrabajador","%"+ nombreTrabajador.replaceAll("ñ", "n") + "%").ignoreCase()).add(Restrictions.like("nombretrabajador","%"+ nombreTrabajador.replaceAll("n", "ñ") + "%").ignoreCase()));
         }
         return c.list();
     }
+    
 
     @Log
     Object onActionFromSeleccionaTitular(Trabajador traba) {
