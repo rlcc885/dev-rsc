4178167932947c36cd51b7365a9ee8df39c21ca1
renatochavezgirao@gmail.com <renatochavezgirao@gmail.com@7438076b-1481-9fcf-951e-ee4c66ea697b>
2012-09-10 17:47:35 +0000

    RCHG

--- trunk/servir/src/main/java/helpers/Constantes.java
+++ trunk/servir/src/main/java/helpers/Constantes.java
@@ -31,7 +31,8 @@ public class Constantes {
    public static final String TRABAJADOR_EXITO ="Trabajador agregado exitosamente.";
    public static final String TRABAJADOR_EDIT_EXITO ="Trabajador  modificado exitosamente.";
    public static final String CARGO_EXITO ="Cargo creado /modificado exitosamente.";
-   public static final String CARGO_ASIGNADO_EXITO ="Cargo asignado / modificado exitosamente.";
+   public static final String CARGO_ASIGNADO_EXITO ="Cargo Asignado modificado exitosamente.";
+   public static final String CARGO_ASIGNADO_BAJA ="Trabajador dado de Baja exitosamente.";
    public static final String MERITO_DEMERITO_EXITO ="Mérito demérito creado / modificado exitosamente.";
    public static final String UNIDAD_ORGANICA_EXITO ="Unidade Organica creada / modificada exitosamente.";
    
