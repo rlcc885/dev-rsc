61376a8ed114971fbac7e35cc75077f0cd7b02fb
jurguenzambrano@gmail.com <jurguenzambrano@gmail.com@7438076b-1481-9fcf-951e-ee4c66ea697b>
2012-09-17 23:10:44 +0000

    wilson actualizando

---
+++ servir/src/main/java/com/tida/servir/pages/ConsultaSanciones.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tida.servir.pages;
+
+import com.tida.servir.base.GeneralPage;
+import com.tida.servir.components.Envelope;
+import com.tida.servir.entities.*;
+import com.tida.servir.services.CargosSelectModel;
+import com.tida.servir.services.GenericSelectModel;
+import helpers.Constantes;
+import helpers.Encriptacion;
+import helpers.Helpers;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import org.apache.tapestry5.ComponentResources;
+
+/**
+ *
+ * @author Miller Cribillero
+ * @date 17/09/2012 18:00
+ * 
+ */
+public class ConsultaSanciones extends GeneralPage {
+    
+}
