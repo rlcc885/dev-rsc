5f078f19630a2617dc079b2bf621082a5fda5f64
renatochavezgirao@gmail.com <renatochavezgirao@gmail.com@7438076b-1481-9fcf-951e-ee4c66ea697b>
2012-09-18 20:05:30 +0000

    RCHG

--- servir/src/main/java/Batch/Helpers/ListLineaEntidadUE.java
+++ servir/src/main/java/Batch/Helpers/ListLineaEntidadUE.java
@@ -5,6 +5,7 @@
 
 package Batch.Helpers;
 
+import Batch.Tratamiento;
 import au.com.bytecode.opencsv.CSVReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -13,6 +14,10 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import jxl.Sheet;
+import jxl.Workbook;
 
 /**
  *
@@ -23,169 +28,177 @@ public class ListLineaEntidadUE {
     public static List<LineasArchivos> getListLineaEntidadUE(String path, ArrayList<String> misCSVs) throws FileNotFoundException, IOException{
         List<LineasArchivos> llo = new LinkedList<LineasArchivos>();
         LineasArchivos lo;
-       
-        if(!misCSVs.get(14).equals("")){
-            lo = new LineasArchivos();
-            lo.setLineas(lineaArchivo(path.concat(misCSVs.get(14).concat(".csv"))));
-            lo.setArchivo("Antecedent Laboral");
-            llo.add(lo);
+        try {
+            if(!misCSVs.get(0).equals("")){
+                Workbook wb = Workbook.getWorkbook(new File(path+"/"+misCSVs.get(0)+".xls"));
+                lo = new LineasArchivos();
+                Sheet sheet = wb.getSheet(0);
+                int rows = sheet.getRows();
+                lo = new LineasArchivos();
+                lo.setLineas(rows-1);
+                lo.setArchivo("xxxENTIDAD");
+                llo.add(lo);
+            }
+            if(!misCSVs.get(1).equals("")){
+                Workbook wb = Workbook.getWorkbook(new File(path+"/"+misCSVs.get(1)+".xls"));
+                lo = new LineasArchivos();
+                Sheet sheet = wb.getSheet(0);
+                int rows = sheet.getRows();
+                lo = new LineasArchivos();
+                lo.setLineas(rows-1);
+                lo.setArchivo("xxxCONCEPTO");
+                llo.add(lo);
+            }
+            if(!misCSVs.get(2).equals("")){
+                Workbook wb = Workbook.getWorkbook(new File(path+"/"+misCSVs.get(2)+".xls"));
+                lo = new LineasArchivos();
+                Sheet sheet = wb.getSheet(0);
+                int rows = sheet.getRows();
+                lo = new LineasArchivos();
+                lo.setLineas(rows-1);
+                lo.setArchivo("xxxUNIDADORGANICA");
+                llo.add(lo);
+            }
+            if(!misCSVs.get(3).equals("")){
+                Workbook wb = Workbook.getWorkbook(new File(path+"/"+misCSVs.get(3)+".xls"));
+                lo = new LineasArchivos();
+                Sheet sheet = wb.getSheet(0);
+                int rows = sheet.getRows();
+                lo = new LineasArchivos();
+                lo.setLineas(rows-1);
+                lo.setArchivo("xxxCARGO");
+                llo.add(lo);
+            }
+            if(!misCSVs.get(4).equals("")){
+                Workbook wb = Workbook.getWorkbook(new File(path+"/"+misCSVs.get(4)+".xls"));
+                lo = new LineasArchivos();
+                Sheet sheet = wb.getSheet(0);
+                int rows = sheet.getRows();
+                lo = new LineasArchivos();
+                lo.setLineas(rows-1);
+                lo.setArchivo("xxxCARGOASIGNADO");
+                llo.add(lo);
+            }
+            if(!misCSVs.get(5).equals("")){
+                Workbook wb = Workbook.getWorkbook(new File(path+"/"+misCSVs.get(5)+".xls"));
+                lo = new LineasArchivos();
+                Sheet sheet = wb.getSheet(0);
+                int rows = sheet.getRows();
+                lo = new LineasArchivos();
+                lo.setLineas(rows-1);
+                lo.setArchivo("xxxREMUNERACION");
+                llo.add(lo);
+            }
+            if(!misCSVs.get(6).equals("")){
+                Workbook wb = Workbook.getWorkbook(new File(path+"/"+misCSVs.get(6)+".xls"));
+                lo = new LineasArchivos();
+                Sheet sheet = wb.getSheet(0);
+                int rows = sheet.getRows();
+                lo = new LineasArchivos();
+                lo.setLineas(rows-1);
+                lo.setArchivo("xxxEVALUACION");
+                llo.add(lo);
+            }
+            if(!misCSVs.get(7).equals("")){
+                Workbook wb = Workbook.getWorkbook(new File(path+"/"+misCSVs.get(7)+".xls"));
+                lo = new LineasArchivos();
+                Sheet sheet = wb.getSheet(0);
+                int rows = sheet.getRows();
+                lo = new LineasArchivos();
+                lo.setLineas(rows-1);
+                lo.setArchivo("xxxCONSTANCIA");
+                llo.add(lo);
+            }
+            if(!misCSVs.get(8).equals("")){
+                Workbook wb = Workbook.getWorkbook(new File(path+"/"+misCSVs.get(8)+".xls"));
+                lo = new LineasArchivos();
+                Sheet sheet = wb.getSheet(0);
+                int rows = sheet.getRows();
+                lo = new LineasArchivos();
+                lo.setLineas(rows-1);
+                lo.setArchivo("xxxTRABAJADOR");
+                llo.add(lo);
+            }
+            if(!misCSVs.get(9).equals("")){
+                Workbook wb = Workbook.getWorkbook(new File(path+"/"+misCSVs.get(9)+".xls"));
+                lo = new LineasArchivos();
+                Sheet sheet = wb.getSheet(0);
+                int rows = sheet.getRows();
+                lo = new LineasArchivos();
+                lo.setLineas(rows-1);
+                lo.setArchivo("xxxESTUDIO");
+                llo.add(lo);
+            }
+            if(!misCSVs.get(10).equals("")){
+                Workbook wb = Workbook.getWorkbook(new File(path+"/"+misCSVs.get(10)+".xls"));
+                lo = new LineasArchivos();
+                Sheet sheet = wb.getSheet(0);
+                int rows = sheet.getRows();
+                lo = new LineasArchivos();
+                lo.setLineas(rows-1);
+                lo.setArchivo("xxxCURSO");
+                llo.add(lo);
+            }
+            if(!misCSVs.get(11).equals("")){
+                Workbook wb = Workbook.getWorkbook(new File(path+"/"+misCSVs.get(11)+".xls"));
+                lo = new LineasArchivos();
+                Sheet sheet = wb.getSheet(0);
+                int rows = sheet.getRows();
+                lo = new LineasArchivos();
+                lo.setLineas(rows-1);
+                lo.setArchivo("xxxANTECEDENTE");
+                llo.add(lo);
+            }
+            if(!misCSVs.get(12).equals("")){
+                Workbook wb = Workbook.getWorkbook(new File(path+"/"+misCSVs.get(12)+".xls"));
+                lo = new LineasArchivos();
+                Sheet sheet = wb.getSheet(0);
+                int rows = sheet.getRows();
+                lo = new LineasArchivos();
+                lo.setLineas(rows-1);
+                lo.setArchivo("xxxPRODUCCION");
+                llo.add(lo);
+            }
+            if(!misCSVs.get(13).equals("")){
+                Workbook wb = Workbook.getWorkbook(new File(path+"/"+misCSVs.get(13)+".xls"));
+                lo = new LineasArchivos();
+                Sheet sheet = wb.getSheet(0);
+                int rows = sheet.getRows();
+                lo = new LineasArchivos();
+                lo.setLineas(rows-1);
+                lo.setArchivo("xxxFAMILIAR");
+                llo.add(lo);
+            }
+            if(!misCSVs.get(14).equals("")){
+                Workbook wb = Workbook.getWorkbook(new File(path+"/"+misCSVs.get(14)+".xls"));
+                lo = new LineasArchivos();
+                Sheet sheet = wb.getSheet(0);
+                int rows = sheet.getRows();
+                lo = new LineasArchivos();
+                lo.setLineas(rows-1);
+                lo.setArchivo("xxxDEMERITO");
+                llo.add(lo);
+            }            
+        }catch(Exception ioe) {
+            Logger.getLogger(Tratamiento.class.getName()).log(Level.SEVERE, null, ioe);
+            System.out.println(""+ ioe.getMessage());
         }
         
-        if(!misCSVs.get(19).equals("")){
-            lo = new LineasArchivos();
-            lo.setLineas(lineaArchivo(path.concat(misCSVs.get(19).concat(".csv"))));
-            lo.setArchivo("Ausencia y Licencia");
-            llo.add(lo);
-        }
-            
-        if(!misCSVs.get(6).equals("")){
-            lo = new LineasArchivos();
-            lo.setLineas(lineaArchivo(path.concat(misCSVs.get(6).concat(".csv"))));
-            lo.setArchivo("Cargo");
-            llo.add(lo);
-        }
-            
-        if(!misCSVs.get(9).equals("")){
-            lo = new LineasArchivos();
-            lo.setLineas(lineaArchivo(path.concat(misCSVs.get(9).concat(".csv"))));
-            lo.setArchivo("Cargo Asignado");
-            llo.add(lo);
-        }
-            
-        if(!misCSVs.get(12).equals("")){
-            lo = new LineasArchivos();
-            lo.setLineas(lineaArchivo(path.concat(misCSVs.get(12).concat(".csv"))));
-            lo.setArchivo("Certificacion");
-            llo.add(lo);
-        }
-            
-        if(!misCSVs.get(1).equals("")){
-            lo = new LineasArchivos();
-            lo.setLineas(lineaArchivo(path.concat(misCSVs.get(1).concat(".csv"))));
-            lo.setArchivo("Concepto Remunerativo");
-            llo.add(lo);
-        }
-            
-        if(!misCSVs.get(20).equals("")){
-            lo = new LineasArchivos();
-            lo.setLineas(lineaArchivo(path.concat(misCSVs.get(20).concat(".csv"))));
-            lo.setArchivo("Constancia Documental");
-            llo.add(lo);
-        }
-            
-        if(!misCSVs.get(13).equals("")){
-            lo = new LineasArchivos();
-            lo.setLineas(lineaArchivo(path.concat(misCSVs.get(13).concat(".csv"))));
-            lo.setArchivo("Curso");
-            llo.add(lo);
-        }
-            
-        if(!misCSVs.get(2).equals("")){
-            lo = new LineasArchivos();
-            lo.setLineas(lineaArchivo(path.concat(misCSVs.get(2).concat(".csv"))));
-            lo.setArchivo("Esquema Remunerativa");
-            llo.add(lo);
-        }
-            
-        if(!misCSVs.get(18).equals("")){
-            lo = new LineasArchivos();
-            lo.setLineas(lineaArchivo(path.concat(misCSVs.get(18).concat(".csv"))));
-            lo.setArchivo("Evaluacion");
-            llo.add(lo);
-        }
-            
-        if(!misCSVs.get(10).equals("")){
-            lo = new LineasArchivos();
-            lo.setLineas(lineaArchivo(path.concat(misCSVs.get(10).concat(".csv"))));
-            lo.setArchivo("Familiar");
-            llo.add(lo);
-        }
-            
-        if(!misCSVs.get(8).equals("")){
-            lo = new LineasArchivos();
-            lo.setLineas(lineaArchivo(path.concat(misCSVs.get(8).concat(".csv"))));
-            lo.setArchivo("Legajo");
-            llo.add(lo);
-        }
-            
-        if(!misCSVs.get(15).equals("")){
-            lo = new LineasArchivos();
-            lo.setLineas(lineaArchivo(path.concat(misCSVs.get(15).concat(".csv"))));
-            lo.setArchivo("Merito Demerito");
-            llo.add(lo);
-        }
-            
-        if(!misCSVs.get(0).equals("")){
-            lo = new LineasArchivos();
-            lo.setLineas(lineaArchivo(path.concat(misCSVs.get(0).concat(".csv"))));
-            lo.setArchivo("Entidad Unidad Ejecutora");
-            llo.add(lo);
-        }
-            
-        /*if(!misCSVs.get(4).equals("")){
-            lo = new LineasArchivos();
-            lo.setLineas(lineaArchivo(path.concat(misCSVs.get(4).concat(".csv"))));
-            lo.setArchivo("Organo");
-            llo.add(lo);
-        }*/
-            
-        if(!misCSVs.get(3).equals("")){
-            lo = new LineasArchivos();
-            lo.setLineas(lineaArchivo(path.concat(misCSVs.get(3).concat(".csv"))));
-            lo.setArchivo("Plantilla Remunerativa");
-            llo.add(lo);
-        }
-            
-        if(!misCSVs.get(16).equals("")){
-            lo = new LineasArchivos();
-            lo.setLineas(lineaArchivo(path.concat(misCSVs.get(16).concat(".csv"))));
-            lo.setArchivo("Produccion Intelectual");
-            llo.add(lo);
-        }
-            
-        if(!misCSVs.get(17).equals("")){
-            lo = new LineasArchivos();
-            lo.setLineas(lineaArchivo(path.concat(misCSVs.get(17).concat(".csv"))));
-            lo.setArchivo("Remuneracion Personal");
-            llo.add(lo);
-        }
-            
-        if(!misCSVs.get(11).equals("")){
-            lo = new LineasArchivos();
-            lo.setLineas(lineaArchivo(path.concat(misCSVs.get(11).concat(".csv"))));
-            lo.setArchivo("Titulo");
-            llo.add(lo);
-        }
-            
-        if(!misCSVs.get(7).equals("")){
-            lo = new LineasArchivos();
-            lo.setLineas(lineaArchivo(path.concat(misCSVs.get(7).concat(".csv"))));
-            lo.setArchivo("Trabajador");
-            llo.add(lo);
-        }
-            
-        if(!misCSVs.get(5).equals("")){
-            lo = new LineasArchivos();
-            lo.setLineas(lineaArchivo(path.concat(misCSVs.get(5).concat(".csv"))));
-            lo.setArchivo("Unidad Organica");
-            llo.add(lo);
-        }
-                    
         return llo;
     }
     
-     public static int lineaArchivo(String archivo) throws FileNotFoundException, IOException {
-
-        int linea = 0;
-
-        File file = new File(archivo);
-        CSVReader reader = new CSVReader(new FileReader(file), '|');
-        String[] nextLine;
-        while ((nextLine = reader.readNext()) != null) {
-            if(nextLine.length>2)
-                linea++;
-        }
-        return linea;
-    }
+//     public static int lineaArchivo(String archivo) throws FileNotFoundException, IOException {
+//
+//        int linea = 0;
+//
+//        File file = new File(archivo);
+//        CSVReader reader = new CSVReader(new FileReader(file), '|');
+//        String[] nextLine;
+//        while ((nextLine = reader.readNext()) != null) {
+//            if(nextLine.length>2)
+//                linea++;
+//        }
+//        return linea;
+//    }
 
 }
---
+++ servir/src/main/java/Batch/Helpers/ValidacionXLS.java
@@ -0,0 +1,159 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Batch.Helpers;
+
+import Batch.Tratamiento;
+import com.tida.servir.entities.Usuario;
+import java.io.File;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+import org.hibernate.Session;
+import jxl.*;
+import java.io.*;
+import java.util.LinkedList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author arson
+ */
+public class ValidacionXLS {
+    private ArrayList<String> misCSVs = new ArrayList<String>();
+    public ArrayList<String> archivos = new ArrayList<String>();
+    
+    String path;
+    String origenArchivo;
+    Session session;
+    List<String> errores;
+    Usuario usuario;
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public ArrayList<String> getMisCSVs() {
+        return misCSVs;
+    }
+
+    public String getOrigenArchivo() {
+        return origenArchivo;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public Session getSession() {
+        return session;
+    }
+    
+    public ValidacionXLS(String path, String origenArchivo, Session session, List<String> errores, Usuario usuario) throws IOException, ParseException {
+        this.path = path;
+        this.origenArchivo = origenArchivo;
+        this.session = session;
+        this.errores = errores;
+        this.usuario = usuario;
+        
+        archivos.add("xxxENTIDAD");
+        archivos.add("xxxCONCEPTO");
+        archivos.add("xxxCARGO");
+        archivos.add("xxxUNIDADORGANICA");
+        archivos.add("xxxCARGOASIGNADO");
+        archivos.add("xxxREMUNERACION");
+        archivos.add("xxxEVALUACION");
+        archivos.add("xxxCONSTANCIA");
+        archivos.add("xxxTRABAJADOR");
+        archivos.add("xxxESTUDIO");
+        archivos.add("xxxCURSO");
+        archivos.add("xxxANTECEDENTE");
+        archivos.add("xxxPRODUCCION");
+        archivos.add("xxxFAMILIAR");
+        archivos.add("xxxDEMERITO");
+        
+        for (int k = 0; k < 15; k++) {
+            misCSVs.add("");
+        }
+        
+        errores = listArchivosDepositorioClassified(path);
+        
+        if(usuario.getRolid()!=2){
+            if (misCSVs.get(0).equals("")) {
+                errores.add("Archivo xxxENTIDAD no encontrado");
+                return;
+            }
+        }
+        
+        if(usuario.getRolid()==2){
+            if(misCSVs.get(0).equals("xxxENTIDAD")){                
+               errores.add("Archivo xxxENTIDAD no esta permitido de subir"); 
+               return;
+            }
+            if(misCSVs.get(5).equals("xxxREMUNERACION")){ 
+               errores.add("Archivo xxxREMUNERACION no esta permitido de subir"); 
+               return;
+            }
+            if(misCSVs.get(1).equals("xxxCONCEPTO")){ 
+               errores.add("Archivo xxxCONCEPTO no esta permitido de subir"); 
+               return;
+            }
+        }
+        
+        
+    }
+    
+    public List<String> listArchivosDepositorioClassified(String path) throws IOException {
+        File directorio = new File(path);
+        File[] list = directorio.listFiles();
+        if (list != null) {
+            for (int i = 0; i < list.length; i++) {
+                if (list[i].getName().endsWith(".XLS") || list[i].getName().endsWith(".xls")) {
+                    misCSVs.set(compareStringDepositorioToArrayListArchivos(list[i].getName().substring(0, (list[i].getName().length()) - 4), archivos, errores), list[i].getName().substring(0, (list[i].getName().length()) - 4));
+                }
+            }
+        } else {
+            errores.add(directorio + " : Problema con el directorio");
+        }
+        return errores;
+
+    }
+    
+    public static int compareStringDepositorioToArrayListArchivos(String depositorio, ArrayList archivo, List<String> errores) {
+
+        int posicion = 0;
+
+        posicion = archivo.indexOf(depositorio);
+        if (posicion == -1) {
+            errores.add("Error en el nombre del archivo.");
+            return 0;
+        }
+        
+        return posicion;
+    }
+    public List<LineasArchivos> getCantLineasArchivos(List<String> errores) {
+        List<LineasArchivos> llo = new LinkedList<LineasArchivos>();
+        try {
+            llo = ListLineaEntidadUE.getListLineaEntidadUE(path, misCSVs);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Tratamiento.class.getName()).log(Level.SEVERE, null, ex);
+            errores.add("Error buscando archivo: " + ex.getMessage());
+        } catch (IOException ex) {
+            Logger.getLogger(Tratamiento.class.getName()).log(Level.SEVERE, null, ex);
+            errores.add("Error procesando archivo: " + ex.getMessage());
+        }
+        return llo;
+    }
+    
+    
+    
+    
+       
+    
+}
