21e64c3e3a981c9afaa7e44856daea60b7c65e77
renatochavezgirao@gmail.com <renatochavezgirao@gmail.com@7438076b-1481-9fcf-951e-ee4c66ea697b>
2012-10-10 21:09:14 +0000

    RCHG

---
+++ servir/src/main/java/com/tida/servir/entities/LkSumaFecha.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tida.servir.entities;
+
+import java.util.Date;
+import java.io.Serializable;
+import javax.persistence.*;
+import org.apache.tapestry5.beaneditor.NonVisual;
+
+/**
+ *
+ * @author Jurguen Zambrano
+ */
+
+public class LkSumaFecha implements Serializable {    
+    
+    public LkSumaFecha() {
+    }
+    @Temporal(TemporalType.DATE)
+    private Date fecha;
+    @Id
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+    
+    
+    
+    
+}
